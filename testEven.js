29e2a5a8938edba612a867e0437a6e0e436f88c1
RYANNNHZ <riantojelek871@gmail.com>
2024-09-22 12:52:45 +0700

    add testEven

---
+++ testEven.js
@@ -0,0 +1,7 @@
+const testEven = (n) => {return n % 2 === 0 ? true : false;}
+
+console.log(testEven(0))
+console.log(testEven(0.5))
+console.log(testEven(1))
+console.log(testEven(2))
+console.log(testEven(-4))
